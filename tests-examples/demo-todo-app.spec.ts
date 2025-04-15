d6e0e7f5e8d2eeab8c9432d712769e4cc7b3dde7
junseong <junseongday@gmail.com>
2025-04-15 22:41:39 +0900

    wip: test

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -29,7 +29,7 @@ test.describe('New Todo', () => {
     await newTodo.press('Enter');
 
     // Make sure the list now has two todo items.
-    await expect(page.getByTestId('todo-title')).toHaveText([
+    await expect(page.getByTestId('todo-title123')).toHaveText([
       TODO_ITEMS[0],
       TODO_ITEMS[1]
     ]);
